d1e8adf0f4d7f656ff6d5e68384451275b3aec5d
gosvoh <vohmina2011@yandex.ru>
2020-09-19 14:52:19 +0300

    Fixed output for calculator, now if you input roman it will also print in roman

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,6 +11,8 @@
  * Калькулятор умеет работать только с арабскими или римскими цифрами одновременно, при вводе пользователем строки вроде 3 + II
  * калькулятор должен выбросить исключениеи прекратить свою работу.
  *
+ * При вводе римских чисел, ответ должен быть выведен римскими цифрами, соответственно, при вводе арабских - ответ ожидается арабскими.
+ *
  * При вводе пользователем неподходящих чисел приложение выбрасывает исключение и завершает свою работу.
  *
  * При вводе пользователем строки, не соответствующей одной из вышеописанных арифметических операций, приложение выбрасывает исключение и завершает свою работу.
@@ -49,26 +51,26 @@ public class Calculator {
 
             if (isInteger(input[0])) val1 = Integer.parseInt(input[0]);
             else {
-                val1 = RomanNums.romanToArabic(input[0]);
+                val1 = Nums.romanToArabic(input[0]);
                 isArabic = false;
             }
 
             operation = input[1];
 
             if (isInteger(input[2])) {
-                if (!isArabic)
-                    throw new RuntimeException("Вы ввели разные типы чисел, не надо так :(");
+                if (!isArabic) throw new RuntimeException("Вы ввели разные типы чисел, не надо так :(");
                 val2 = Integer.parseInt(input[2]);
             } else {
-                if (isArabic)
-                    throw new RuntimeException("Вы ввели разные типы чисел, не надо так :(");
-                val2 = RomanNums.romanToArabic(input[2]);
+                if (isArabic) throw new RuntimeException("Вы ввели разные типы чисел, не надо так :(");
+                val2 = Nums.romanToArabic(input[2]);
             }
 
             if (val1 < 1 || val1 > 10 || val2 < 1 || val2 > 10)
                 throw new RuntimeException("Числа должны быть от 1 до 10 включительно!");
 
-            System.out.println(CalculationProcess.getCalculationResult(val1, val2, operation));
+            int result = CalculationProcess.getCalculationResult(val1, val2, operation);
+            if (isArabic) System.out.println(result);
+            else System.out.println(Nums.arabicToRoman(result));
         }
 
     }
